84f2debc52c3019403e18748d71355d5e361c584
amaar <amaar.ahmednur@student.moringaschool.com>
2023-09-22 23:46:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  const names = ["Charlie", "Samip", "Ali"];
+  const eventName = "birthday";
+  const thankYouMessages = writeCards(names, eventName);
+  
+  console.log(thankYouMessages);
+  
+  function countDown(startingNumber) {
+    let i = startingNumber;
+  
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+  
+  countDown(10); // Call the function to count down from 10
+  
